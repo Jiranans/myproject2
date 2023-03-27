d8fdaea2f2a0fc30555153935504fcab8765576b
Jiranan <baimonjiranan@gmail.com>
2023-03-27 15:07:23 +0700

    commit

--- myproject2.java
+++ myproject2.java
@@ -2,5 +2,6 @@ public class myproject2 {
     public static void main(String[] args) {
         System.out.println("Lodchong java");
         System.out.println("Lodchong java");
+        System.out.println("Lodchong java");
     }
 }
